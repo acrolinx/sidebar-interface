257cb35b54ad83686fefb9a536dd94be3d25f427
Ahmed Abdelsalam <ahmed.abdelsalam@acrolinx.com>
2022-02-04 15:52:21 +0100

    DEV-27236: Updated comments for documentation

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@
  */
 
 /**
- * This document describes the interface of the Acrolinx Sidebar with Batch checks. Batch checks are supported from sidebar version 15.0 and platform version 2022.01
+ * This document describes the interface of the Acrolinx Sidebar. Batch checking feature is only supported from sidebar version 15.0 and platform version 2022.01.
  *
  * Let's understand how the typical bootstrapping of an integration and the Acrolinx Sidebar works:
  *
